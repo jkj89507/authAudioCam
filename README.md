49cca1c432715449b26568d3dfd0019ba88ab3d7
jkj89507 <JKJ89507@gmail.com>
2022-03-22 04:38:51 +0300

    firstTry

---
+++ README.md
@@ -0,0 +1,12 @@
+# authAudioCam
+
+Самое главное - после выкачки проекта скачать архив rus.rar по ссылке:
+
+https://drive.google.com/drive/folders/1MpChG_N_Bz3VyfIQqp8DkPrsug0GYT5F?usp=sharing
+
+и в директории <path to project>\authAudioCam\venv\Lib\site-packages\speech_recognition\pocketsphinx-data\en-US
+разархивировать файлы вне папки с таким же названием как и у архива, иначе все поломается. Т.е. структура должна быть вот такая:
+  ![image](https://user-images.githubusercontent.com/46351111/159387222-2b4e57a8-c289-47f3-a428-df9da9e11910.png)
+
+если понадобится английский-словарь для распознавания речи, по этому же пути и с такими же условиями разархивировать 
+en-US.rar
--- venv/main.py
+++ venv/main.py
@@ -1,1 +1,44 @@
-print("hello world")
+import speech_recognition as speachRec
+from pocketsphinx import LiveSpeech, get_model_path
+import time
+import os
+
+model_path = get_model_path()
+
+startTime = time.time()
+stopTime = 0
+
+microphone = speachRec.Microphone()
+recognizer = speachRec.Recognizer()
+
+with microphone as audioFile:
+    print("Say something...")
+    recognizer.adjust_for_ambient_noise(audioFile)
+    audio = recognizer.listen(audioFile)
+
+    try:
+        print("Total speeach: ")
+        file = open('total.txt', 'w')
+        str = ""
+        for i in recognizer.recognize_sphinx(audio):
+            str += i
+        print(str)
+        file.write(str)
+        stopTime = time.time()
+        spendTime = int(stopTime - startTime)
+        sec = spendTime % 60
+        min = spendTime // 60
+        print(min, "m", sec, "s")
+    except Exception as error:
+        print("Erorr is ", str(error))
+#
+# speech = LiveSpeech(
+#     verbose=False,
+#     sampling_rate=16000,
+#     buffer_size=2048,
+#     no_search=False,
+#     full_utt=False,
+#     hmm=os.path.join(model_path, 'zero_ru.cd_cont_4000'),
+#     lm=os.path.join(model_path, 'ru.lm'),
+#     dic=os.path.join(model_path, 'ru.dic')
+# )
